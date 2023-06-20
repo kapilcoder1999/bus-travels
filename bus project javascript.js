0814e23ed0cd15fd425ffe55bb1e9ed6b7782b19
kapilcoder1999 <127014863+kapilcoder1999@users.noreply.github.com>
2023-06-20 22:04:21 +0530

    Add files via upload

---
+++ bus project javascript.js
@@ -0,0 +1,33 @@
+function icons1(){
+    var a=confirm("do you want to visit facebook page");
+
+    if(a==true){
+        window.open("https://www.facebook.com/kapil.pawar.792197");
+        return true;
+    }
+    else{
+        alert("user dont want to visit page")
+    }
+}
+function icons2(){
+    var a=confirm("do you want to visit instagram page");
+
+    if(a==true){
+        window.open("https://www.instagram.com/kapil.pawar007/");
+        return true;
+    }
+    else{
+        alert("user dont want to visit page")
+    }
+}
+function icons3(){
+    var a=confirm("do you want to visit twitter page");
+
+    if(a==true){
+        window.open("https://twitter.com/");
+        return true;
+    }
+    else{
+        alert("user dont want to visit page")
+    }
+}
